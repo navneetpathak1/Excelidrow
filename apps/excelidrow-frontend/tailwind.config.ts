60e7a039aee793563bb817228459a933cc48bd78
navneetpathak1 <navneetpathak1909@gmail.com>
2025-08-25 23:41:16 +0530

    frontend

---
+++ apps/excelidrow-frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {},
+  plugins: [],
+} satisfies Config;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const [roomId, setRoomId] = useState("");
+  const router = useRouter();
+
   return (
-    <div className="min-h-screen flex items-center justify-center text-3xl font-bold underline">
-       Tailwind is working!
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw"
+    }}>
+      <div>
+        <input style={{
+          padding: 10
+        }} value={roomId} onChange={(e) => {
+          setRoomId(e.target.value);
+        }} type="text" placeholder="Room id"></input>
+
+        <button style={{padding: 10}} onClick={() => {
+          router.push(`/room/${roomId}`);
+        }}>Join room</button>
+      </div>
     </div>
   );
 }
